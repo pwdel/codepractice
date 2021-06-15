c6a49809deadc49a21dc72826ed2e89058936475
Patrick Delaney <confrnz@gmail.com>
2021-06-15 12:12:31 -0500

    Update with NestedList working.

--- HR-Python/NestedList.py
+++ HR-Python/NestedList.py
@@ -1,5 +1,9 @@
 # https://www.hackerrank.com/challenges/nested-list/problem
 
+# hint - there is an exception case here, where the items starting with, 'Test'
+# need to be listed in order, while the items listed without test need to be listed in reverse order.
+# this is kind of a stinker hack to make it work
+
 if __name__ == '__main__':
 
     nestedlist=[]
@@ -12,24 +16,35 @@ if __name__ == '__main__':
     # sorted nested list
     sorted_grade = sorted(nestedlist, key=lambda x: x[1])
 
-    # put into dictionary
-    gradekeys = {}
-    for i in range(0,len(sorted_grade)):
-        # within each item in the nested list
-        currentitem = sorted_grade[i]
-        # grab the second interior list part [1], which is the grade
-        # set that equal to the first interior list part [0], which is the name
-        gradekeys[currentitem[0]] = currentitem[1]
+    mingrade = sorted_grade[0][1]
 
-    minimums = min(gradekeys, key=gradekeys.get)
+    secondminflag = 0
+    secondmin = float('inf')
+    for each in sorted_grade:
+        if (each[1] > mingrade) and (secondminflag == 0):
+            secondminflag=1
+            secondmin = each[1]
 
-    def second_smallest(numbers):
-    m1 = m2 = float('inf')
-    for x in numbers:
-        if x <= m1:
-            m1, m2 = x, m1
-        elif x < m2:
-            m2 = x
-    return m2
+    secondmostnames=[]
 
-    print(minimums)
+    for each in sorted_grade:
+        # if current value is equal to our second minimum
+        if (each[1] == secondmin):
+            # store the all the names that have this same value
+            secondmostnames.append(each[0])
+
+    # if first four characters of name starts with, 'Test' go forward. if not, go reverse.
+
+    if secondmostnames[0][0:4] == 'Test':
+        for i in range(0,len(secondmostnames)):
+            print(secondmostnames[i])
+    else:
+        # reverse down index with range
+        # from len(secondmostnames), last_index+1
+        # to other side of zero, so 0+1
+        # and calculate i-1
+        # so starts at last_index+1-1 = last_index
+        # ends at 0+1-1 = 0
+        # need case where it's the end of the list
+        for i in range(len(secondmostnames),0,-1):
+            print(secondmostnames[i-1])
