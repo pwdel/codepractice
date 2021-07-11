ed18f14417541724feb6512a56fd8e60a77389cf
Patrick Delaney <confrnz@gmail.com>
2021-07-10 19:08:26 -0500

    Adding CollectionsCounter.py

---
+++ HR-Python/CollectionsCounter.py
@@ -0,0 +1,19 @@
+# https://www.hackerrank.com/challenges/collections-counter/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+N = input()
+
+print(N)
+
+# input is as follows, need to seperate it out line by line
+
+# 10
+# 2 3 4 5 6 8 7 6 5 18
+# 6
+# 6 55
+# 6 45
+# 6 55
+# 4 40
+# 18 60
+# 10 50
