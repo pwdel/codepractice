52388369c41463e4a83a3fd8ae2f406f20d7d91a
Patrick Delaney <confrnz@gmail.com>
2021-07-21 08:29:58 -0500

    Several updated answers.

--- HR-SQL/revisingtheselectquery.sql
+++ HR-SQL/revisingtheselectquery.sql
@@ -37,12 +37,29 @@ FROM STATION;
 -- https://www.w3schools.com/mysql/func_mysql_mod.asp
 -- MOD(X,N) gives the remainder of X/N
 -- Note that it's not mod(id/2), it's seperated by a comma
--- Ensure
+-- https://www.w3schools.com/sql/sql_distinct.asp
+-- SELECT DISTINCT is used to get individual values
 
-SELECT CITY
+SELECT DISTINCT(CITY)
 FROM STATION
-WHERE MOD(ID,2) = 0;
+WHERE (ID%2) = 0;
 
+
+
+-- Query the Name of any student in STUDENTS who scored higher than
+-- Marks. Order your output by the last three characters of each name.
+-- If two or more students both have names ending in the same last
+-- three characters (i.e.: Bobby, Robby, etc.),
+-- secondary sort them by ascending ID.
+-- Note - https://www.w3schools.com/sql/func_sqlserver_char.asp
+-- char
+
+
+SELECT NAME
+FROM STUDENTS
+WHERE MARKS > 75
+--
+ORDER BY NAME ASC;
 
 -- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
 -- note you can use operation ORDER BY, ASC, DESC
