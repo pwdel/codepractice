f2e6843b009488f3eec3cd6c07c3652d8861a5b7
Patrick Delaney <confrnz@gmail.com>
2021-07-30 22:29:33 -0500

    update.

---
+++ HR-Javascript/LetConst.js
@@ -0,0 +1,50 @@
+// https://www.hackerrank.com/challenges/js10-let-and-const/problem
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    // declare Pi
+    const PI = Math.PI
+    // read r as input
+    let r = readLine();
+    // compute area
+    let area = Math.pow(r,2)*PI;
+    // Print the area of the circle:
+    console.log(area)
+    // compute perimeter
+    let perimeter = 2*PI*r;
+    // Print the perimeter of the circle:
+    console.log(perimeter)
+
+    try {
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
